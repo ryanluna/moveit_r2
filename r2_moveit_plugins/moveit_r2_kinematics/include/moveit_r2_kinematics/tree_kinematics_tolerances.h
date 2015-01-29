ced5f0d676025e10712010db830b71dcb52466b7
Ryan Luna <rluna@rice.edu>
2015-01-29 16:22:52 -0600

    Update angular tolerances

--- r2_moveit_plugins/moveit_r2_kinematics/include/moveit_r2_kinematics/tree_kinematics_tolerances.h
+++ r2_moveit_plugins/moveit_r2_kinematics/include/moveit_r2_kinematics/tree_kinematics_tolerances.h
@@ -52,12 +52,12 @@ namespace moveit_r2_kinematics
     /// \brief Medium priority task linear tolerance
     const double MEDIUM_PRIO_LINEAR_TOL = 1.0;
     /// \brief Medium priority task angular tolerance
-    const double MEDIUM_PRIO_ANGULAR_TOL = 1.0;
+    const double MEDIUM_PRIO_ANGULAR_TOL = 0.5;
 
     /// \brief Low priority task linear tolerance
     const double LOW_PRIO_LINEAR_TOL = 3.0;
     /// \brief Low priority task angular tolerance
-    const double LOW_PRIO_ANGULAR_TOL = 3.0;
+    const double LOW_PRIO_ANGULAR_TOL = 1.0;
 
     /// \brief The maximum allowed joint rotational velocity
     const double MAX_JOINT_VELOCITY = 5.0;
